fcb814d4bd9c8184e2915ac7af124de45bb2c6d3
absaleem <saleem.mcstn@gmail.com>
2022-10-17 20:33:49 +0530

    Add files via upload
    router updated

---
+++ Router/employeeRouter.js
@@ -0,0 +1,13 @@
+const express=require("express");
+const router=express.Router();
+const employeeModule= require("../modules/Employees");
+
+router.get("/get",employeeModule.getEmployees);
+
+router.put("/update/:id",employeeModule.updateEmployees);
+
+router.post("/create",employeeModule.createEmployees);
+
+router.delete("/delete/:id",employeeModule.deleteEmployees);
+
+module.exports=  router;
